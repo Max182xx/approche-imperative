738285a2e329c2d93a08aacdbcf61937e0971c9f
Max182xx <m.garciapro34@gmail.com>
2024-10-02 12:41:05 +0200

    TP02

---
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -0,0 +1,21 @@
+package fr.declaration.variable;
+
+public class DeclarationApp {
+
+    public static void main(String[] args) {
+
+        int var = 0;
+        short zero = 0;
+        int numb = 2;
+        long twelve = 12;
+        char charo = 'b';
+        boolean ahaha = true;
+        String a = "Hello";
+
+        System.out.println( a );
+
+        String randomString = "Voici le résultat d’un calcul : \n1+5=6";
+        System.out.println( randomString  );
+    }
+
+}
--- src/fr/diginamic/Bonjour.java
+++ src/fr/diginamic/Bonjour.java
@@ -1,3 +1,5 @@
+package fr.diginamic;
+
 public class Bonjour {
     public static void main(String[] args) {
         System.out.println("Bonjour tout le monde");
